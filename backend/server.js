98f4bb3815d882a72f383d201d02de97f5fcf39a
Anjali Pujala <anjalipujala003@gmail.com>
2025-08-11 12:41:14 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 const User = require('./models/Users');
 const connectDB = require('./database');
