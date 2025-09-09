bfbd8057bee479ac518fc10c99a300ade1b1f97d
Anjali Pujala <anjalipujala003@gmail.com>
2025-09-09 17:03:47 +0530

    vlogs apis

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const CommentSchema = new mongoose.Schema({
     vlogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vlog', required: true },
     userEmail: { type: String, required: true },
+    userName: { type: String, required: true },
     text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
--- backend/models/Like.js
+++ backend/models/Like.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const LikeSchema = new mongoose.Schema({
     vlogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vlog', required: true },
     userEmail: { type: String, required: true },
+    userName: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
